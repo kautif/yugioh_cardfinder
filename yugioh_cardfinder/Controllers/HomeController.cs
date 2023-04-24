0434cf533e4743df1d5aeb9cf17ba6734c9e4b42
Autif Kamal <kautif@gmail.com>
2023-04-24 12:49:07 -0500

    initialized views and api

---
+++ yugioh_cardfinder/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace yugioh_cardfinder.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        public IActionResult Index()
+        {
+            string ygoJSON = getYGOData();
+            return View((object)ygoJSON);
+        }
+
+        private string getYGOData()
+        {
+            var client = new HttpClient();
+            string searchTerm = "dragon";
+            var ygoURL = $"https://db.ygoprodeck.com/api/v7/cardinfo.php?fname={searchTerm}";
+            var ygoResponse = client.GetStringAsync(ygoURL).Result;
+            Console.WriteLine(ygoResponse);
+            var ygoJSON = JsonObject.Parse(ygoResponse)["data"].ToString();
+            return ygoJSON;
+        }
+    }
+}
---
+++ yugioh_cardfinder/Models/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace yugioh_cardfinder.Models
+{
+    public class Card
+    {
+        public string name {  get; set; }
+        public string desc { get; set; }
+        public string type { get; set; }
+        public string race { get; set; }
+        public string imageURL { get; set; }
+    }
+}
